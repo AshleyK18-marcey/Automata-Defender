adab5c5d01425a936862bcf46c150c2943302ffb
Ashley Kastler <akastler18@gmail.com>
2024-11-27 12:18:16 -0700

    add existing code

---
+++ src/app/Components/MovingNumbers.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from '../CSS/MovingNumbers.module.css';
+
+const MovingNumbers = (): JSX.Element => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.numbers}>
+
+        </div>
+    </div>
+  );
+};
+
+export default MovingNumbers;
